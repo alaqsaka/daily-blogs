3508cbed431d30481a471704338d56a94f21b9a2
alaqsaka <aqsaabidin15@gmail.com>
2022-01-10 20:49:58 +0700

    chore: removing client submodule

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
-
+// tes
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
