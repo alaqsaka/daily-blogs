d923b141af1f940a86b9f62459d473aeaa197dd9
alaqsaka <aqsaabidin15@gmail.com>
2022-01-10 20:42:32 +0700

    setup

---
+++ server/routes/posts.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { getPosts, createPost } from "../controllers/posts.js";
+
+const router = express.Router();
+
+// https://localhost:5000/posts
+// posts = prefix
+router.get("/", getPosts);
+router.post("/", createPost);
+
+export default router;
