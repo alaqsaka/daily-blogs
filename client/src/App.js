f4b28ca52e9a76ed0f7efb6a188c294ca9f27ca4
alaqsaka <aqsaabidin15@gmail.com>
2022-01-10 21:01:49 +0700

    feat: adding client folder

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { getPosts } from "./actions/posts";
+import memories from "./images/memories.png";
+import { useDispatch } from "react-redux";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+
+const App = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  return (
+    <Container maxidth="lg">
+      <AppBar position="static" color="inherit" className={classes.appBar}>
+        <Typography className={classes.heading} variant="h2" align="center">
+          CS Stud Blogs
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
