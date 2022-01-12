a2b77b7543c254a2e62936f43463c6be61945823
alaqsaka <aqsaabidin15@gmail.com>
2022-01-12 14:53:25 +0700

    feat: create card components with material-ui, add data to card

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
@@ -9,11 +10,24 @@ const Posts = () => {
 
   console.log(posts);
   return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+    // If there is no post (length == 0) shows CircularProgress
+    // Else shows posts
+    !posts.length ? (
+      <CircularProgress />
+    ) : (
+      <Grid
+        className={classes.container}
+        container
+        alignItems="strech"
+        spacing={3}
+      >
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
 };
 
